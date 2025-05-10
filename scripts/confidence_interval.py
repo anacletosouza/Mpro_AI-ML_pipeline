ae87f48c1d8bdde3382b3efc87e1ce9cfe5847b7
Anacleto Silva de Souza <127983309+anacletosouza@users.noreply.github.com>
2025-05-10 17:32:15 -0300

    Update confidence_interval.py

--- scripts/confidence_interval.py
+++ scripts/confidence_interval.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.stats import t
 
-def t_student_interval(df,n):
-    df = df.copy()  
-    df["t_critical"] = df["n"].apply(lambda n: t.ppf(0.975, df=n - 1))
-    df["IC95_inf"] = df["mean"] - df["t_critical"] * df["sd"] / np.sqrt(df["n"])
-    df["IC95_sup"] = df["mean"] + df["t_critical"] * df["sd"] / np.sqrt(df["n"])
+def t_student_interval(df, n):
+    df = df.copy()
+    t_critical = t.ppf(0.975, df=n - 1)
+    df["t_critical"] = t_critical
+    df["IC95_inf"] = df["mean"] - t_critical * df["sd"] / np.sqrt(df["n"])
+    df["IC95_sup"] = df["mean"] + t_critical * df["sd"] / np.sqrt(df["n"])
     df["interval_length"] = df["IC95_sup"] - df["IC95_inf"]
     return df
+
